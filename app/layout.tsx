8809884d1b66c05ad17d103535dfa3b98f0e718a
kormaphon <freddy.orma92@gmail.com>
2025-09-19 10:15:02 -0500

    Changes description
    Changes description and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'SerpText',
+  description: 'Cotizaciones industriales Inducom',
   generator: 'v0.app',
 }
 
