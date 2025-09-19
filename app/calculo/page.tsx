15e2bf0d89ad5738a12a9c545f6169fee279cfe4
kormaphon <freddy.orma92@gmail.com>
2025-09-19 15:31:31 -0500

    New PageCaculate

---
+++ app/calculo/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function calculoPage(){
+    return(
+    <main className="px-4 py-10">
+        <h1 className="text-3xl font-bold">Nuevo cálculo</h1>
+        <p className="opacity-80">Página lista para trabajar aquí.</p>
+    </main>
+    );
+}
