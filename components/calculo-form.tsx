6642604663a3621958538a31cefbd1c09c4b5b56
kormaphon <freddy.orma92@gmail.com>
2025-09-19 17:17:04 -0500

    Form 1.1

--- components/calculo-form.tsx
+++ components/calculo-form.tsx
@@ -1,8 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { use, useState } from "react";
 
 export default function CalculoForm(){
+  
+    const [nombreCompleto, setNombreCompleto] = useState("");
+    
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
         alert("Form send...");
@@ -10,7 +13,24 @@ export default function CalculoForm(){
 
     return(
         <form onSubmit={handleSubmit}>
-            <button type="submit">Send Data</button>
+           
+        <label className="block">
+            <span className="text-sm font-medium">Nombres completos del Cliente</span>
+            
+            <input 
+                type="text" 
+                id="nombre_completo" 
+                name="nombre_completo" 
+                value={nombreCompleto}
+                onChange={(e) => setNombreCompleto(e.target.value)}
+                 placeholder="Juan Peréz"
+                autoComplete="name"
+                className="mt-1 block w-full rounded border px-3 py-2"
+                required 
+            />
+        </label>
+
+        <button type="submit">Send Data</button>
         </form>
     )
 }
