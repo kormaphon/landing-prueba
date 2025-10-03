f05102807034fa2d8fce16e3513af075904d2548
kormaphon <freddy.orma92@gmail.com>
2025-10-03 11:35:42 -0500

    page

--- app/calculo/page.tsx
+++ app/calculo/page.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import CalculoForm from "@/components/calculo-form";
 
-export default function calculoPage(){
+export default function     calculoPage(){
     return(
     <main className="px-4 py-10">
         <div className="mt-6 rounded-lg border p-4">
