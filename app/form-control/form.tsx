4d85845c9e8fc5b0bbb8a47e8d19af93cd3798ff
kormaphon <freddy.orma92@gmail.com>
2025-10-03 11:08:33 -0500

    creation component form-control

---
+++ app/form-control/form.tsx
@@ -0,0 +1,14 @@
+
+import FormControlApp from "@/components/form-control";
+import React from "react";
+
+
+export default function calculoPage(){
+    return(
+    <main className="px-4 py-10">
+        <div className="mt-6 rounded-lg border p-4">
+            <FormControlApp/>
+        </div>
+    </main>
+    );
+}
