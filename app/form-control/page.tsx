591d0d5c4fd28323878fedd37f9bd17331ba55f3
kormaphon <freddy.orma92@gmail.com>
2025-10-03 11:23:20 -0500

    page

--- app/form-control/page.tsx
+++ app/form-control/page.tsx
@@ -3,7 +3,7 @@ import FormControlApp from "@/components/form-control";
 import React from "react";
 
 
-export default function calculoPage(){
+export default function FormPage(){
     return(
     <main className="px-4 py-10">
         <div className="mt-6 rounded-lg border p-4">
